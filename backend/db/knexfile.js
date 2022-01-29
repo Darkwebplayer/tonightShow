9523d3c5d1013a6032ef30a5f846f4840b595a4b
darkwebplayer <amrithanadhm@gmail.com>
2022-01-30 01:16:11 +0530

    check db config

--- backend/db/knexfile.js
+++ backend/db/knexfile.js
@@ -1,7 +1,7 @@
 // Update with your config settings.
-const parse = require("pg-connection-string").parse;
-const pgconfig = parse(process.env.DATABASE_URL);
-pgconfig.ssl = { rejectUnauthorized: false };
+// const parsepgConnection = require("pg-connection-string").parse;
+// const pgConfig = parsepgConnection(process.env.DATABASE_URL);
+// pgconfig.ssl = { rejectUnauthorized: false };
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
@@ -33,7 +33,10 @@ module.exports = {
 
   production: {
     client: "postgresql",
-    connection: pgconfig,
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: false,
+    },
     pool: {
       min: 2,
       max: 10,
