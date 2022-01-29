a44a715417ef1a04e9f2984220f61057b3b39a55
darkwebplayer <amrithanadhm@gmail.com>
2022-01-29 16:42:18 +0530

    setuped clientAddress

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,13 @@
 const express = require("express");
 const app = express();
-
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
 const cors = require("cors");
 const corsOptions = {
-  // origin: ["http://localhost:3000", "https://192.168.1.7:3000"],
-  origin: ["*"],
+  origin: [process.env.CLIENT_ADDRESS],
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
-const cookieParser = require("cookie-parser");
-require("dotenv").config();
 const userRoute = require("./routes/users");
 const movieRoute = require("./routes/movies");
 const authRoute = require("./routes/authentication");
