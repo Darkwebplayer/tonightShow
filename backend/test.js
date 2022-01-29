9dbd6ee702bc7fc0fba095006ec4dd75490055ae
darkwebplayer <amrithanadhm@gmail.com>
2022-01-29 21:36:55 +0530

    test imdbapi

--- backend/test.js
+++ backend/test.js
@@ -1,10 +1,56 @@
-const IMDb = require("imdb-light");
+// const IMDb = require("imdb-light");
 require("dotenv").config();
-const Dao = require("./dao/movie");
+// const Dao = require("./dao/movie");
 
-/*
-GET TOP 250
-*/
+// /*
+// GET TOP 250
+// */
+// function fetch() {
+//   return new Promise(function (resolve, reject) {
+//     IMDb.topMovies((details) => {
+//       resolve(details);
+//     });
+//   });
+// }
+
+// async function getTop() {
+//   let movies = await fetch();
+
+//   return new Promise((resolve, reject) => {
+//     let mov = [];
+//     movies.forEach((movie) => {
+//       mov.push(movie.id);
+//     });
+//     resolve(mov);
+//   });
+// }
+
+// const imdb = require("imdb-api");
+
+// const insertIntoTable = async (id) => {
+//   let { rating, imdbid, name } = await imdb
+//     .get({ id: id }, { apiKey: `${process.env.IMDB_API_KEY}` })
+//     .then((res) => res)
+//     .catch((err) => {
+//       console.log(err);
+//     });
+//   Dao.addMovie(imdbid, name, rating, "");
+// };
+// // insertIntoTable();
+// const getTop250 = async () => {
+//   getTop()
+//     .then((res) => {
+//       res.forEach((id) => {
+//         insertIntoTable(id);
+//       });
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// };
+// getTop250();
+const imdb = require("imdb-api");
+const IMDb = require("imdb-light");
 function fetch() {
   return new Promise(function (resolve, reject) {
     IMDb.topMovies((details) => {
@@ -12,40 +58,13 @@ function fetch() {
     });
   });
 }
-
-async function getTop() {
-  let movies = await fetch();
-
-  return new Promise((resolve, reject) => {
-    let mov = [];
-    movies.forEach((movie) => {
-      mov.push(movie.id);
-    });
-    resolve(mov);
-  });
-}
-
-const imdb = require("imdb-api");
-
-const insertIntoTable = async (id) => {
-  let { rating, imdbid, name } = await imdb
-    .get({ id: id }, { apiKey: `${process.env.IMDB_API_KEY}` })
-    .then((res) => res)
-    .catch((err) => {
-      console.log(err);
-    });
-  Dao.addMovie(imdbid, name, rating, "");
+const Movies = async () => {
+  let movie = await fetch();
+  let movieid = movie[0];
+  let { rating, imdbid, name } = await imdb.get(
+    { id: movieid.id },
+    { apiKey: process.env.IMDB_API_KEY }
+  );
+  console.log(rating, imdbid, name);
 };
-// insertIntoTable();
-const getTop250 = async () => {
-  getTop()
-    .then((res) => {
-      res.forEach((id) => {
-        insertIntoTable(id);
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-};
-getTop250();
+Movies();
