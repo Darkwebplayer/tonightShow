f862f9a2e3439581a93fe36ab05c398f1ec7b55e
darkwebplayer <amrithanadhm@gmail.com>
2022-01-31 15:28:36 +0530

    console log crap

--- frontend/src/components/Home/Movie.vue
+++ frontend/src/components/Home/Movie.vue
@@ -64,6 +64,7 @@ export default {
     onMounted(() => {
       console.log("mounted");
       const element = canvas.value;
+      console.log(element);
       domtoimage
         .toPng(element)
         .then((dataurl) => {
