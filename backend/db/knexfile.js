97eca2f18e8f032e2a02d2d182d511eb4eee8ff8
darkwebplayer <amrithanadhm@gmail.com>
2022-02-10 22:05:10 +0530

    dburl changed

--- backend/db/knexfile.js
+++ backend/db/knexfile.js
@@ -35,8 +35,7 @@ module.exports = {
   production: {
     client: "postgresql",
     connection: {
-      connectionString:
-        "postgres://pztyooyuzwxfgt:caa68bf3c272126289ee05cde81e3580d4106f718f268e318414ff190af4663b@ec2-18-209-169-66.compute-1.amazonaws.com:5432/d2fo63cjktekk2",
+      connectionString: `${process.env.DATABASE_URL}`,
       ssl: { rejectUnauthorized: false },
     },
     pool: {
